aa3b9f69db3bedcc07984f5dd94ed881517d5299
Juan Pablo Henao Gonzalez <146042547+ghpjjphg@users.noreply.github.com>
2026-03-16 02:08:11 -0500

    Update database_config.py

--- database_config.py
+++ database_config.py
@@ -2,10 +2,13 @@ import os
 from dotenv import load_dotenv
 import mysql.connector
 
-conn = mysql.connector.connect(
-    host=os.getenv("ballast.proxy.rlwy.net"),
+load_dotenv()
+
+def get_connection():
+    return mysql.connector.connect(
+        host=os.getenv("ballast.proxy.rlwy.net"),
     port=int(os.getenv("28843")),
     user=os.getenv("root"),
     password=os.getenv("ftkwrcyxjPORSuENUHimDvgtOYOvyJlu"),
     database=os.getenv("railway")
-)
+    )
