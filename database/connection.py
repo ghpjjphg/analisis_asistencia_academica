6027bd77ddb52afb2343dfd9a3aa99b48d934c41
Juan Pablo Henao Gonzalez <146042547+ghpjjphg@users.noreply.github.com>
2026-03-16 02:27:08 -0500

    Update connection.py

--- database/connection.py
+++ database/connection.py
@@ -3,10 +3,9 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.getenv("MYSQLHOST"),
-        port=int(os.getenv("MYSQLPORT")),
-        user=os.getenv("MYSQLUSER"),
-        password=os.getenv("MYSQLPASSWORD"),
-        database=os.getenv("MYSQLDATABASE"),
-        connection_timeout=10
+        host=os.environ["MYSQLHOST"],
+        port=int(os.environ["MYSQLPORT"]),
+        user=os.environ["MYSQLUSER"],
+        password=os.environ["MYSQLPASSWORD"],
+        database=os.environ["MYSQLDATABASE"]
     )
